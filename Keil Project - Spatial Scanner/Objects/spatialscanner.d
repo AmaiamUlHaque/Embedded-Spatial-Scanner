ba2f6e39e476e50ce9bed66206fb5fe4e1257a38
Amaiam Haque <haque.amaiam5@gmail.com>
2025-03-23 11:23:25 -0400

    added base files

---
+++ Keil Project - Spatial Scanner/Objects/spatialscanner.d
@@ -0,0 +1,3 @@
+./objects/spatialscanner.o: SpatialScanner.c PLL.h SysTick.h uart.h \
+  onboardLEDs.h tm4c1294ncpdt.h VL53L1X_api.h vl53l1_platform.h \
+  vl53l1_types.h
---
+++ Keil Project - Spatial Scanner/SpatialScanner.c
@@ -0,0 +1,14 @@
+//Project 2 Deliverable
+//Amaiam Ul Haque
+//March 31, 2025 - April 1, 2025
+
+
+
+//HEADER FILES --------------------------------------------------------------------------------------------------------
+#include <stdint.h>
+#include "PLL.h"
+#include "SysTick.h"
+#include "uart.h"
+#include "onboardLEDs.h"
+#include "tm4c1294ncpdt.h"
+#include "VL53L1X_api.h"
